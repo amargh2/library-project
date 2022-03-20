629ffdd97358f6de6f51941ce162690f30ee3bc7
amargh2 <margherioa@gmail.com>
2022-03-20 13:39:49 -0500

    button to add books now works, fields are cleared automatically when add button is pushed, button populates with each book added; still need to get delete book button working; needs to be styled; but js is relatively functional

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Declaration of important variables, including DOM inputs*/
+/* Declaration of important variables, including DOM inputs/areas*/
 
 let library = []
 
@@ -7,12 +7,7 @@ let titleField = document.getElementById('title').value
 let authorField = document.getElementById('author').value
 let pagesField = document.getElementById('pages').value
 let tagField = document.getElementById('tag').value
-let readField = document.getelementbyID
-
-/* Just a test of concept 
-function info() {
-    return `${title} by ${author}, ${pages} pages, ${read}`
-} */
+let libraryField = document.querySelector('.library').textContent
 
 /*constructor function */
 
@@ -22,21 +17,32 @@ function Book(title, author, pages, tag) {
     this.pages = pages
 }
 
-/*Add book to library function.*/
+/*Add book to library array function.*/
 
-function addBookToLibrary(tag) {
-    
-    library.push(tag)
+function addBookToLibrary(book) {
+    library.push(book)
 }
 
 /*Button event listener - when you click it calls the constructor
- and adds the book to the library array */
+ and adds the book to the library array and changes the html -
+ adds div for the library book and creates the delete button for the element*/
 
 addButton.addEventListener('click', () => {
     let book = new Book(titleField, authorField, pagesField)
-    
+    var newDiv = document.createElement('div');
+    newDiv.className = 'library-card'
+    newButton = document.createElement('button')
+    newButton.className = 'library-card-button'
+    buttonText = document.createTextNode('Delete Book')
+    newDiv.textContent =`${book.title}, ${book.author}, ${book.pages}`
+    document.querySelector('.library').appendChild(newDiv)
+    newDiv.appendChild(newButton)
+    newButton.appendChild(buttonText)
     addBookToLibrary(book)
+    clearAll()
 })
+
+/*Functions that allow the text inputs to dynamically update */
 
 function handleChangeTitle() {
     titleField = document.getElementById('title').value
@@ -47,9 +53,27 @@ function handleChangeAuthor() {
 }
 
 function handleChangePages() {
-    pagesField = document.getElementById('pages')
+    pagesField = document.getElementById('pages').value
 }
 
 function handleChangeTag() {
-    tagField = document.getElementById('tag')
+    tagField = document.getElementById('tag').value
 }
+
+/*Clear all inputs after the add to library button is pushed*/
+
+function clearAll() {
+    document.getElementById('title').value = ""
+    document.getElementById('author').value = ""
+    document.getElementById('pages').value = ""
+    document.getElementById('tag').value = ""
+}
+
+function deleteBook() {
+    deleteButton = document.querySelector('.library-card-button')
+    deleteButton.parentElement.remove()
+}
+
+deleteButton.addEventListener('click', () => {
+    deleteBook()
+})
