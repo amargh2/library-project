7dded820df14797f771f74b1dcd92afc867e8b32
amargh2 <margherioa@gmail.com>
2022-03-25 18:03:47 -0500

    Got the code working, and the delete buttons now delete the proper corresponding book. Just need to style (inspiration? goodreads?), and then all done.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # library-project
 library
+This is a library app assigned as a learning project from The Odin Project. It tests ability to create event listeners, forms, DOM manipulation, and using more advanced selectors, including data selectors. 
+
+When the user inputs a book, it is displayed on the screen with a delete button that allows them to delete the book.
--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function Book(title, author, pages, tag) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.tag = tag
 }
 
 /*Add book to library array function.*/
@@ -23,24 +24,64 @@ function addBookToLibrary(book) {
     library.push(book)
 }
 
+/*Clear all inputs after the add to library button is pushed*/
+
+function clearAll() {
+    document.getElementById('title').value = ""
+    document.getElementById('author').value = ""
+    document.getElementById('pages').value = ""
+    document.getElementById('tag').value = ""
+}
+
+/*This function creates the delete button that will appear when the book is added; uses data index id (which is derived
+from length of the library array) to ensure that the button deletes the correct book (ie, the one it appears with) */
+ function newButton() {
+    button = document.createElement('button');
+    text = document.createTextNode('Delete Book');
+    button.dataset.indexNumber = library.length;
+    button.appendChild(text);
+    return button
+}
+
+/*adds event listener to each created button -- matches data-index of the button with data-index of corresponding
+div, and removes*/
+function buttonFunction(button) {
+    button.addEventListener ('click', () => {
+       divToSelect = document.querySelector(`[data-index-number = "${button.dataset.indexNumber}"]`)
+       divToSelect.remove()
+})
+}
+
+/*new div creator function -- assigns class name for styling and data index number for proper deletion handling */
+function newDiv () {
+    let bookDiv = document.createElement('div'); 
+    bookDiv.className = 'library-card';
+    bookDiv.dataset.indexNumber = library.length;
+    return bookDiv;
+}
+
+/*function that ties most everything together -- adds button, book div, and appends everything*/
+function addCard (title, author, pages) {
+    libraryDiv = document.querySelector('.library')
+    bookDiv = newDiv();
+    bookDiv.textContent = `${title}, ${author}, ${pages}`
+    libraryDiv.appendChild(bookDiv);
+    button = newButton()
+    buttonFunction(button)
+    bookDiv.appendChild(button)
+
+}
 /*Button event listener - when you click it calls the constructor
  and adds the book to the library array and changes the html -
  adds div for the library book and creates the delete button for the element*/
-
 addButton.addEventListener('click', () => {
     let book = new Book(titleField, authorField, pagesField)
-    var newDiv = document.createElement('div');
-    newDiv.className = 'library-card'
-    newButton = document.createElement('button')
-    newButton.className = 'library-card-button'
-    buttonText = document.createTextNode('Delete Book')
-    newDiv.textContent =`${book.title}, ${book.author}, ${book.pages}`
-    document.querySelector('.library').appendChild(newDiv)
-    newDiv.appendChild(newButton)
-    newButton.appendChild(buttonText)
     addBookToLibrary(book)
+    addCard(book.title, book.author, book.pages)
     clearAll()
 })
+
+
 
 /*Functions that allow the text inputs to dynamically update */
 
@@ -60,20 +101,9 @@ function handleChangeTag() {
     tagField = document.getElementById('tag').value
 }
 
-/*Clear all inputs after the add to library button is pushed*/
 
-function clearAll() {
-    document.getElementById('title').value = ""
-    document.getElementById('author').value = ""
-    document.getElementById('pages').value = ""
-    document.getElementById('tag').value = ""
-}
-
+/* function for the event listener on each book's delete book button */
 function deleteBook() {
     deleteButton = document.querySelector('.library-card-button')
     deleteButton.parentElement.remove()
 }
-
-deleteButton.addEventListener('click', () => {
-    deleteBook()
-})
