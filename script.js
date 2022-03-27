c7d699e1af826e10933425c462fe2462e6890906
amargh2 <margherioa@gmail.com>
2022-03-27 18:50:49 -0500

    got the buttons to toggle using javascript and event delegation (first time learning about it! very cool!)

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let titleField = document.getElementById('title').value
 let authorField = document.getElementById('author').value
 let pagesField = document.getElementById('pages').value
 let libraryField = document.querySelector('.library').textContent
+let libraryDiv = document.querySelector('.library')
 let readField = document.getElementById('read').value
 
 /*constructor function */
@@ -34,8 +35,11 @@ function clearAll() {
 }
 
 /*This function creates the delete button that will appear when the book is added; uses data index id (which is derived
-from length of the library array) to ensure that the button deletes the correct book (ie, the one it appears with) */
- function newButton() {
+from length of the library array) to ensure that the button deletes the correct book 
+(ie, the one it appears with) */
+
+
+function deleteButton() {
     button = document.createElement('button');
     text = document.createTextNode('Delete Book');
     button.dataset.indexNumber = library.length;
@@ -44,8 +48,50 @@ from length of the library array) to ensure that the button deletes the correct 
     return button
 }
 
+/*creates a check mark or x button based on results of read check */
+
+function makeReadButton() {
+    readButton = document.createElement('button');
+    checkIcon = document.createElement('i');
+    checkIcon.className = 'fa-solid fa-check'
+    readButton.className = 'read'
+    text = document.createTextNode(' Read')
+    readButton.appendChild(checkIcon)
+    readButton.appendChild(text)
+    return readButton 
+}
+
+function makeNotReadButton() {
+    notReadButton = document.createElement('button');
+    notReadIcon = document.createElement('i')
+    notReadIcon.className = 'fa-solid fa-x'
+    notReadButton.className = 'not-read'
+    text = document.createTextNode('  Not Read')
+    notReadButton.appendChild(notReadIcon)
+    notReadButton.appendChild(text)
+    return notReadButton
+}
+
+/*function for event listener when the read/unread button is clicked
+function readButtonClick {
+    
+} */
+
+/*function that checks whether the answer to read? is yes or no; calls appropriate button maker
+function based on answer */
+
+function checkYesOrNo(read) {
+    if (read == 'yes') {
+        return makeReadButton()
+    }
+    else {
+        return makeNotReadButton()
+    }
+}
+
 /*adds event listener to each created button -- matches data-index of the button with data-index of corresponding
 div, and removes*/
+
 function buttonFunction(button) {
     button.addEventListener ('click', () => {
        divToSelect = document.querySelector(`[data-index-number = "${button.dataset.indexNumber}"]`)
@@ -54,6 +100,7 @@ function buttonFunction(button) {
 }
 
 /*new div creator function -- assigns class name for styling and data index number for proper deletion handling */
+
 function newDiv () {
     let bookDiv = document.createElement('div'); 
     bookDiv.className = 'library-card';
@@ -61,23 +108,54 @@ function newDiv () {
     return bookDiv;
 }
 
+/*function checkButtonEvent(checkButton) {
+    checkButton.addEventListener('click', () => {
+        if (checkButton.className == 'read') {
+            checkButton.childNodes[0].className = 'fa-solid fa-x'
+            checkButton.childNodes[1].textContent = '  Not Read'
+        }
+        else {
+            checkButton.childNodes[0].className = 'fa-solid fa-check'
+            checkButton.childNodes[1].textContent = ' Read'
+        }
+    })
+}*/
+
+libraryDiv.addEventListener('click', function (e) {
+    if (e.target.classList.contains('read')) {
+        e.target.childNodes[0].className = 'fa-solid fa-x';
+        e.target.childNodes[1].textContent = ' Not Read';
+        e.target.classList = 'not-read'
+    }
+    else if (e.target.classList.contains('not-read')){
+        e.target.childNodes[0].className = 'fa-solid fa-check'
+        e.target.childNodes[1].textContent = ' Read'
+        e.target.className = 'read'
+    }
+    else {
+        return false
+    }
+})
+
 /*function that ties most everything together -- adds button, book div, and appends everything*/
 function addCard (title, author, pages, read) {
     libraryDiv = document.querySelector('.library')
     bookDiv = newDiv();
-    bookDiv.textContent = `${title}, ${author}, ${pages}, ${read}`
+    bookDiv.textContent = `${title}, ${author}, ${pages} pages`
     libraryDiv.appendChild(bookDiv);
-    button = newButton()
+    button = deleteButton()
     buttonFunction(button)
     bookDiv.appendChild(button)
-
+    checkButton = checkYesOrNo(read)
+    //checkButtonEvent(checkButton)//
+    bookDiv.appendChild(checkButton)
 }
 
 
-/*This function checks that each field in the form is filled out*/
+/*This function checks that each field in the form is filled out; if not, returns a false value that prevents
+completion of the listener function on the add button*/
 function validateForm() {
     if (titleField == null || titleField == "", authorField == null || authorField == "", pagesField == null || pagesField == "", pagesField == null || readField == "not specified") {
-      alert("Make sure you filled out every field!");
       return false;
     }
   }
@@ -86,7 +164,8 @@ function validateForm() {
 /*Button event listener - when you click it calls the constructor
  and adds the book to the library array and changes the html -
  adds div for the library book and creates the delete button for the element*/
-addButton.addEventListener('click', () => {
+
+ addButton.addEventListener('click', () => {
     let book = new Book(titleField, authorField, pagesField, readField)
     if (validateForm() == false) {
         return;
